305a9ee499bf0b870105940819f73ca8e802eee6
Raphael Ella <74969464+raphael656@users.noreply.github.com>
2023-05-04 08:57:14 +0200

    Update IA_flowers.py
    modif des commentaire sur l'app

--- IA_flowers.py
+++ IA_flowers.py
@@ -9,7 +9,7 @@ from flask import Flask, request
 # On definit les chemins d'acces au différentes hyper parametre
 
 
-# Classe permettant de réaliser une prédiction sur une nouvelle donnée
+# Classe permettant de realiser une prediction sur une nouvelle donnee
 
 
 
@@ -24,9 +24,9 @@ def index():
 def predict():
     """
     # Fonction qui permet de convertir une image en array, de charger le modele et de lui injecter notre image pour une prediction
-    :param modelPath: chemin du modèle au format hdf5
-    :param imagePath: chemin de l'image pour realiser une prediction
-    :param imageSize: défini la taille de l'image. IMPORTANT : doit être de la même taille que celle des images
+    :param modelPath: chemin du modele au format hdf5
+    :param imagePath: chemin de l image pour realiser une prediction
+    :param imageSize: défini la taille de l image. IMPORTANT : doit être de la même taille que celle des images
     du dataset d'entrainements
     :param label: nom de nos 5 classes de sortie
     """
@@ -41,9 +41,9 @@ def predict():
     start = time.time()
 
     # Chargement du modele
-    print("Chargement du modèle :\n")
+    print("Chargement du modele :\n")
     model = load_model(modelPath)
-    print("\nModel chargé.")
+    print("\nModel charge.")
 
     #Chargement de notre image et traitement
     data = []
@@ -56,13 +56,13 @@ def predict():
 
     #On reshape pour correspondre aux dimensions de notre modele
     # Arg1 : correspond au nombre d'image que on injecte
-    # Arg2 : correspond a la largeur de l'image
-    # Arg3 : correspond a la hauteur de l'image
-    # Arg4 : correspond au nombre de canaux de l'image (1 grayscale, 3 couleurs)
+    # Arg2 : correspond a la largeur de l image
+    # Arg3 : correspond a la hauteur de l image
+    # Arg4 : correspond au nombre de canaux de l image (1 grayscale, 3 couleurs)
     
     dimension = data[0].shape
 
-    #Reshape pour passer de 3 à 4 dimension pour notre réseau
+    #Reshape pour passer de 3 à 4 dimension pour notre reseau
     data = data.astype(np.float32).reshape(data.shape[0], dimension[0], dimension[1], dimension[2])
 
     #On realise une prediction
@@ -72,7 +72,7 @@ def predict():
     #On recupere le numero de label qui a la plus haut prediction
     maxPredict = np.argmax(prediction)
 
-    #On recupere le mot correspondant à l'indice precedent
+    #On recupere le mot correspondant à l indice precedent
     word = label[maxPredict]
     pred = int(prediction[0][maxPredict] * 100.)
     prediction = str(pred)+ ' %'
