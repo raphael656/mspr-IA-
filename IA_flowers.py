63567036f2fd808da24f828285e5ca74a9b73c67
raphael656 <74969464+raphael656@users.noreply.github.com>
2023-05-05 11:42:07 +0200

    mise a jour

--- IA_flowers.py
+++ IA_flowers.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 import time
 import os
-from flask import Flask, request
+from flask import Flask, request,  jsonify
 
 # On definit les chemins d'acces au différentes hyper parametre
 
@@ -76,9 +76,49 @@ def predict():
     word = label[maxPredict]
     pred = int(prediction[0][maxPredict] * 100.)
     prediction = str(pred)+ ' %'
+    DataFleur  = get_flower_data(word)
+    datafleurs = [prediction]
+    
+    for t in DataFleur : 
+        datafleurs.extend(t)
+        
+    print(datafleurs)
+    #fonction de la recuperation des data de la fleur
+
+
 
   
-    return [word ,prediction]
+    return datafleurs
+import sqlite3
 
+def get_flower_data(flower_name):
+    conn = sqlite3.connect('fleurs.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM Plante WHERE Nom_Plante = ?", (flower_name,))
+    flower_data = c.fetchall()
+    conn.close()
+    return flower_data
+# Afficher les annonce
+@app.route('/Annonce/', methods=['POST'])
+def viewAnnonce():
+    conn = sqlite3.connect('fleurs.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM annonce")
+    annonce_data = c.fetchall()
+    conn.close()
+    return jsonify(annonce_data)
+
+# ajouter une annonce
+@app.route('/Ajouter/', methods=['POST'])
+def addAnnonce():
+    annonce_data = request.json # Récupère les données envoyées dans la requête HTTP POST sous forme de JSON
+    conn = sqlite3.connect('fleurs.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO annonce(Titre_Annonce, Description_Annonce, Adresse1_Annonce, DatePub_Annonce, Lat_Annonce, Long_Annonce) VALUES (?, ?, ?,?,?,?)", (annonce_data['Titre_Annonce'], annonce_data['Description_Annonce'], annonce_data['Adresse1_Annonce'],annonce_data['DatePub_Annonce'], annonce_data['Lat_Annonce'], annonce_data['Long_Annonce']))
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Annonce ajoutée avec succès !'})
+
+    
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port='3400',debug=True)
+      app.run(host='0.0.0.0', port='3401',debug=True)
